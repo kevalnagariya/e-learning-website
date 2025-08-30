53183f324ddab4d140f64f4495a4069ccfdcd591
keval.nagariya27 <kevalnagariya27@gmail.com>
2025-08-30 16:58:52 +0530

    header created

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+        <div className="container">
+            {/* content */}
+            <div className=''>
+                {/* Title */}
+                <div className=''>
+                    <span className=''>
+                        <img src='/images/shape-1.png' alt='title shape' width={39} height={43}></img>
+                    </span>
+
+                    <div className=''>
+                        <span className=''>
+                            <img src='/images/shape-2.png' alt='title shape' width={48} height={48}></img>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            {/* Banner */}
+            <figure>
+
+            </figure>
+        </div>
+
+    </section>
+  )
+}
+
+export default Hero
